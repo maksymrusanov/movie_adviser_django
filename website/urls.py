6cb5d6d73d97576929bec1dff858bcec06862915
maksym <1maksymrusanov1@gmail.com>
2025-09-17 13:20:06 +0000

    add logic for login and registration

--- website/urls.py
+++ website/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login", views.login, name="login"),
+    path("login", views.login_view, name="login"),
     path("register", views.register, name="register"),
     path("", views.main, name="main"),
     path("advises", views.advised, name="advised"),
     path("top_picks", views.top_picks, name="top_picks"),
     path("watched_movies", views.watched_movies, name="watched_movies"),
-    path('logout/', views.logout_view, name='logout'),
+    path('logout/', views.logout_func, name='logout'),
 
 
 
--- website/views.py
+++ website/views.py
@@ -2,17 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 
 
 def main(request):
+    if request.user.is_authenticated:
+        return render(request, 'main.html')
     return render(request, 'main.html')
 
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
-        user = User.objects.get(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-        return redirect('main', user)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            return render(request, 'login_page.html', {'error': 'Invalid username or password'})
+
     return render(request, 'login_page.html')
 
 
@@ -21,9 +31,12 @@ def register(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        user = User.objects.create_user(
+            username=username, email=email, password=password)
 
-        user = User.objects.create_user(request.POST)
-        return redirect('main')  # важно вернуть redirect
+        login(request, user)
+
+        return redirect('main')
 
     return render(request, 'registration_page.html')
 
@@ -40,6 +53,6 @@ def watched_movies(request):
     return render(request, 'watched_movies.html')
 
 
-def logout_view(request):
+def logout_func(request):
     logout(request)
     return redirect('main')
